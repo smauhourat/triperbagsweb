109ecaa0f056a85aeed273d57be7085d4f725882
SMauhourat <santiagomauhourat@hotmail.com>
2022-11-12 16:48:26 -0300

    powered by

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const onClickBody = document.querySelector("body");
+
+function showScreenSize() {
+    console.log('Window Size: ' + window.innerWidth + ' - ' + window.innerHeight);
+}
+
+toggle.addEventListener("click", showScreenSize, false);
